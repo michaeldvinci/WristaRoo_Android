d97b4da93712b9f5e30ff5bdff9aae9eceb344a3
michaeldvinci <michaeldvinci@gmail.com>
2016-04-13 18:00:37 -0400

    WearableListenerService Implementation

--- mobile/src/main/java/com/michaeldvinci/conedmiro/schedaroo/MainActivity.java
+++ mobile/src/main/java/com/michaeldvinci/conedmiro/schedaroo/MainActivity.java
@@ -7,12 +7,10 @@ package com.michaeldvinci.conedmiro.schedaroo;
 
 import android.content.Intent;
 import android.os.Bundle;
-import android.os.SystemClock;
 import android.preference.PreferenceManager;
 import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
-import android.util.Log;
 import android.view.View;
 import android.widget.ArrayAdapter;
 import android.widget.Button;
@@ -23,16 +21,10 @@ import com.google.android.gms.common.api.GoogleApiClient;
 import com.google.android.gms.common.api.PendingResult;
 import com.google.android.gms.wearable.DataApi;
 import com.google.android.gms.wearable.DataMap;
-import com.google.android.gms.wearable.MessageApi;
-import com.google.android.gms.wearable.Node;
-import com.google.android.gms.wearable.NodeApi;
 import com.google.android.gms.wearable.PutDataMapRequest;
 import com.google.android.gms.wearable.PutDataRequest;
 import com.google.android.gms.wearable.Wearable;
 
-import java.io.ByteArrayOutputStream;
-import java.io.DataOutputStream;
-import java.io.IOException;
 import java.util.ArrayList;
 import java.util.HashSet;
 import java.util.Set;
@@ -109,7 +101,7 @@ public class MainActivity extends AppCompatActivity implements
                     System.out.println("[mobile] - StringArray /hopefully/ put");
                     map.putLong(TIME_MESSAGE_PATH, System.currentTimeMillis());
                     System.out.println("[mobile] - Timestamp /hopefully/ put");
-                    PutDataRequest putDataReq = putDataMapReq.asPutDataRequest().setUrgent();
+                    PutDataRequest putDataReq = putDataMapReq.asPutDataRequest();
                     PendingResult<DataApi.DataItemResult> pendingResult = Wearable.DataApi.putDataItem(mGoogleApiClient, putDataReq);
                     System.out.println(putDataReq);
                 }
